44ecf53f90bbce5fe56c7e3e628475672b0623ba
Shepora <alarconchavezluisalejandro@gmail.com>
2025-02-19 11:24:19 -0500

    Salidas por consola

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola Clase");
+        System.out.println("ESTAMOS APRENDIENDO COMANDOS GIT");
+        System.out.println("repasando comandos");
 
 
 
